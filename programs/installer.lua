b9497c9e4d63eac428c62d4bca9aeab474de6189
Shane Yu <shanesimpkins@gmail.com>
2022-05-03 15:27:06 +0100

    feat: improve installer for all devices

--- programs/installer.lua
+++ programs/installer.lua
@@ -1,19 +1,39 @@
 -- Program: Installer
--- Version: 1
+-- Version: 2
 
-local width, height = term.getSize()
-local cursor, offset = 1, 0
 local baseUrl = "https://raw.githubusercontent.com/ShaneYu/mc-computercraft-programs/main/"
 local programsList = {}
+local selectedProgram = 1
+
+local width, height = term.getSize()
+local listWidth = width - 3
+local listHeight = height - 5
+
+local installerName = "ShadowNet Installer"
+local installerVersion = 0
+
+local nameHeading = "Name"
+local versionHeading = "Version  "
+local installedHeading = "Installed  "
+
+if pocket or turtle then
+  listHeight = listHeight - 1
+  versionHeading = "V  "
+  installedHeading = "I  "
+end
 
 local function refreshProgramsList()
-  programsList = textutils.unserialiseJSON(http.get(baseUrl ..'programs/programs.json').readAll())
+  local response = http.get(baseUrl ..'programs/programs.json', { ["Cache-Control"] = "no-cache" })
+
+  programsList = textutils.unserialiseJSON(response.readAll())
+  response.close()
 
   for index, programInfo in pairs(programsList) do
     if fs.exists(programInfo.fileTarget) then
       local fileContent = fs.open(programInfo.fileTarget, "r")
       fileContent.readLine()
       programsList[index].installedVersion = tonumber(string.sub(fileContent.readLine(), 12))
+      fileContent.close()
     end
   end
 end
@@ -29,7 +49,7 @@ local function installProgram(index)
     fs.delete(programInfo.fileTarget)
   end
 
-  local programSource = http.get(string.format("%sprograms/%s", baseUrl, programInfo.fileSource), nil, true).readAll()
+  local programSource = http.get(string.format("%sprograms/%s", baseUrl, programInfo.fileSource), { ["Cache-Control"] = "no-cache" }).readAll()
   local programFile = fs.open(programInfo.fileTarget, "wb")
 
   programFile.write(programSource)
@@ -91,147 +111,116 @@ local function updateAll()
   end
 end
 
-local function updateOffset(lines)
-  if cursor > offset + lines then
-      offset = cursor - lines
-  end
-  if cursor <= offset then
-      offset = cursor - 1
-  end
-end
-
-local function displayListStandard()
+local function displayList()
   local startupProgramPath = getStartupProgramPath()
 
+  for _, programInfo in pairs(programsList) do
+    if programInfo.name == 'Installer' then
+      installerVersion = programInfo.installedVersion or 1
+    end
+  end
+
   term.clear()
-  term.setCursorPos(3, 1)
-  term.write("ShadowNet Installer")
-  term.setCursorPos(2,3)
-  term.setBackgroundColor(colors.white)
+
+  term.setCursorPos(2, 1)
+  term.setTextColor(colors.white)
+  term.setBackgroundColor(colors.black)
+  term.write(installerName)
+  term.setCursorPos(width - #tostring(installerVersion) - 1, 1)
+  term.write("v" .. installerVersion)
+  
+  term.setCursorPos(2, 3)
   term.setTextColor(colors.black)
-  term.write('  Name                      Version   Installed ')
+  term.setBackgroundColor(colors.white)
+  term.write(" Name" .. string.rep(" ", listWidth - #versionHeading - #installedHeading - 4))
+  term.write(versionHeading.. installedHeading)
+
   term.setBackgroundColor(colors.black)
   term.setTextColor(colors.white)
 
-  for line = 1, height - 4 do
-    if (line == cursor - offset) then
-      term.setCursorPos(1, line + 3)
+  local programOffset = 0
+
+  if #programsList > listHeight then
+    programOffset = math.max(selectedProgram - listHeight, 0)
+  end
+
+  for i = 1, math.min(listHeight, #programsList) do
+
+    if (i == selectedProgram - programOffset) then
+      term.setCursorPos(1, i + 3)
       term.write('>')
     end
     
-    if (line + offset <= table.getn(programsList)) then
-      local programInfo = programsList[line + offset]
-      
-      if programInfo.fileTarget == startupProgramPath then
-        term.setCursorPos(2, line + 3)
-        term.write('*')
-      end
-      
-      term.setCursorPos(4, line + 3)
-      term.write(programInfo.name)
-      term.setCursorPos(30, line + 3)
-      term.write(string.format("%d", programInfo.version))
-      
-      if programInfo.installedVersion then
-        term.setCursorPos(40, line + 3)
-        term.write(string.format("%d", programInfo.installedVersion))
-      end
+    local programInfo = programsList[i + programOffset]
+    
+    if programInfo.fileTarget == startupProgramPath then
+      term.setCursorPos(2, i + 3)
+      term.write('*')
+    end
+    
+    term.setCursorPos(3, i + 3)
+    term.write(programInfo.name .. string.rep(" ", listWidth - #versionHeading - #installedHeading - #programInfo.name))
+    term.write(string.format("%d", programInfo.version) .. string.rep(" ", #versionHeading - #tostring(programInfo.version)))
+    
+    if programInfo.installedVersion then
+      term.write(string.format("%d", programInfo.installedVersion))
     end
   end
 
-  term.setCursorPos(4, height)
+  if pocket or turtle then
+    term.setCursorPos(2, height - 1)
+  else
+    term.setCursorPos(2, height)
+  end
+
   term.blit('i', 'f', '0')
   term.write('nstall  ')
+
+  if pocket then
+    term.write("   ")
+  end
+
   term.blit('a', 'f', '0')
   term.write('utostart  ')
-  term.blit('d', 'f', '0')
-  term.write('elete  ')
+
+  if pocket then
+    term.setCursorPos(2, height)
+  end
+
   term.blit('u', 'f', '0')
   term.write('pdate all  ')
-  term.blit('q', 'f', '0')
-  term.write('uit')
-end
 
-function displayPocket()
-  local startupProgramPath = getStartupProgramPath()
+  if turtle then
+    term.setCursorPos(2, height)
+  end
+  
+  if (programsList[selectedProgram].name ~= "Installer") then
+    term.blit('d', 'f', '0')
+    term.write('elete  ')
 
-  term.clear()
-  term.setCursorPos(3,1)
-  term.write('ShadowNet Installer')
-  term.setCursorPos(2,3)
-  term.setBackgroundColor(colors.white)
-  term.setTextColor(colors.black)
-  term.write('                        ')
-  term.setBackgroundColor(colors.black)
-  term.setTextColor(colors.white)
-
-  for line = 1, 8 do
-    if line == cursor - offset then
-      term.setCursorPos(1, line * 3 + 1)
-      term.write('>')
-    end
-
-    if (line + offset <= table.getn(programsList)) then
-      local programInfo = programsList[line + offset]
-
-      if programInfo == startupProgramPath then
-        term.setCursorPos(2, line * 3 + 1)
-        term.write('*')
-      end
-
-      term.setCursorPos(3, line * 3 + 1)
-      term.write(programInfo.name)
-      term.setCursorPos(2, line * 3 + 2)
-      term.setBackgroundColor(colors.white)
-      term.setTextColor(colors.black)
-      term.write('                        ')
-      term.setCursorPos(3, line * 3 + 2)
-      term.write(string.format("Version: %d ", programInfo.version))
-
-      if programInfo.localVersion then 
-        term.write(string.format("Local: %d", programInfo.localVersion))
-      end
-
-      term.setBackgroundColor(colors.black)
-      term.setTextColor(colors.white)
+    if turtle then
+      term.write(" ")
     end
   end
 
-  term.setCursorPos(1, 20)
-  term.blit('i', 'f', '0')
-  term.write('nstall ')
-  term.blit('r', 'f', '0')
-  term.write('emove ')
-  term.blit('u', 'f', '0')
-  term.write('pdate ')
   term.blit('q', 'f', '0')
   term.write('uit')
-end
-
-local function displayList()
-  if pocket then
-    updateOffset(8)
-    displayListPocket()
-  else
-    updateOffset(height - 4)
-    displayListStandard()
-  end
 end
 
 function keyListener()
   while true do
     local event, key = os.pullEvent('key')
 
-    if key == keys.up and cursor > 1 then
-      cursor = cursor - 1
+    if key == keys.up and selectedProgram > 1 then
+      selectedProgram = selectedProgram - 1
     end
 
-    if key == keys.down and cursor < table.getn(programsList) then
-      cursor = cursor + 1
+    if key == keys.down and selectedProgram < #programsList then
+      selectedProgram = selectedProgram + 1
     end
 
     if key == keys.i or key == keys.enter then
-      installProgram(cursor)
+      installProgram(selectedProgram)
     end
 
     if key == keys.u then
@@ -239,11 +228,13 @@ function keyListener()
     end
 
     if key == keys.d or key == keys.delete then
-      uninstallProgram(cursor)
+      if (programsList[selectedProgram].name ~= "Installer") then
+        uninstallProgram(selectedProgram)
+      end
     end
 
     if key == keys.a then
-      toggleStartupProgram(cursor)
+      toggleStartupProgram(selectedProgram)
     end
 
     if key == keys.q then
